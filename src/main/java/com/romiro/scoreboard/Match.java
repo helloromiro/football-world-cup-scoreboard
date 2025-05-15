31e363e01ef9a69706e1b22bdb6489bf06f3f919
Mironov Roman <hello.romiro@gmail.com>
2025-05-15 23:30:29 +0300

    Add Match class with basic fields and constructor

---
+++ src/main/java/com/romiro/scoreboard/Match.java
@@ -0,0 +1,11 @@
+package com.romiro.scoreboard;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+}
