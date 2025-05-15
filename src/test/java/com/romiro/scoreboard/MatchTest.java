3ba0f0ff9444b67e0ce82c3a431cfaff8e04b83b
Mironov Roman <hello.romiro@gmail.com>
2025-05-15 23:34:34 +0300

    Add first example test

---
+++ src/test/java/com/romiro/scoreboard/MatchTest.java
@@ -0,0 +1,13 @@
+package com.romiro.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchTest {
+
+    @Test
+    void testUpdateScore() {
+        Match match = new Match("Mexico", "Canada");
+        // match.updateScore(1, 0);
+    }
+}
